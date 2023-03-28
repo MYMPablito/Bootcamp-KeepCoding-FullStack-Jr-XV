900b65e8170d50234853b8751b220090a2e84099
MYMPablito <pablo-mato@hotmail.com>
2023-03-28 03:09:01 -0300

    primera versión entero a romano con 1994 y 333

--- romanos/main.py
+++ romanos/main.py
@@ -22,62 +22,70 @@ I -> 1
 
 diccionario = {'M':1000, 'D':500, 'C':100, 'L':50, 'X':10, 'V':5, 'I':1}
 
-unidades = {
+dict_enteros_a_romanos = {
     1:'I', 2:'II', 3:'III',
     4:'IV', 5: 'V', 6: 'VI',
     7:'VII', 8: 'VIII', 9:'IX',
-    10: 'X'
-
-}
-
-decenas = {
-    10:'X', 20:'XX', 30:'XXX',
+    10: 'X', 20:'XX', 30:'XXX',
     40:'XL', 50: 'L', 60: 'LX',
-    70:'LXX', 80: 'LXXX', 90:'XC'
-
-}
-
-centenas = {
+    70:'LXX', 80: 'LXXX', 90:'XC',
     100:'C', 200:'CC', 300:'CCC',
     400:'CD', 500: 'D', 600: 'DC',
-    700:'DCC', 800: 'DCCC', 900:'CM'
-
-}
-
-millares = {
+    700:'DCC', 800: 'DCCC', 900:'CM',
     1000:'M', 2000:'MM', 3000:'MMM'
 
 }
 
-#for c,v in diccionario.items():
-#    print(c, "-", v)
+
 
 class RomanNumberError(Exception):
     pass
 
 
 def entero_a_romano(numero):
-    numero = str(numero) #transformar en cadena
+    numero = "{:0>4d}".format(numero) #transformar en cadena
     list_numero = list(numero) #transformar cadena en lista
-    print(list_numero)
+    valor_romano = ""
+    
+    #Optimizamos del for al while:
+    contador = 0
+    valor_num = 1000
+    while contador < len(list_numero):
+        list_numero[contador] = int(list_numero[contador])*valor_num
+        
+        if list_numero[contador] == 0:
+            valor_romano += ""
+        else:
+            valor_romano += dict_enteros_a_romanos.get(list_numero[contador])
+        contador+=1
+        valor_num /=10
 
-    for i in range(0, len(list_numero)):
+
+
+    '''for i in range(0, len(list_numero)):
         print(list_numero[i])
         if i == 0:
             list_numero[i] = int(list_numero[i]) * 1000
-        if i == 1:
+            valor_romano += millares.get(list_numero[i])
+        elif i == 1:
             list_numero[i] = int(list_numero[i]) * 100
-        if i == 2:
+            valor_romano += centenas.get(list_numero[i])
+        elif i == 2:
             list_numero[i] = int(list_numero[i]) * 10
-        if i == 3:
+            valor_romano += decenas.get(list_numero[i])
+        elif i == 3:
             list_numero[i] = int(list_numero[i]) * 1
+            valor_romano += unidades.get(list_numero[i])
+'''
+    
+
+    #for c,v in diccionario.items():
+    #    print(c, "-", v) 
+    
+    return valor_romano
+
+print( entero_a_romano(333) )
 
 
-    #for i in numero:
-    #    print(i)
-    #return "MCMXCIV"
-    return list_numero
-
-print( entero_a_romano(1994) )
 
 
