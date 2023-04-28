6caf98120c9a182847c22b6ef8383afca4ec2c75
MYMPablito <pablo-mato@hotmail.com>
2023-04-28 12:26:24 -0300

    Agregar clase Exchange y su test de funcionalidad

--- consumir-api/test_models.py
+++ consumir-api/test_models.py
@@ -1,5 +1,7 @@
-from appcoints.models import AllCoinApiIo
+from appcoints.models import AllCoinApiIo, Exchange, ModelError
 from appcoints.config import APIKEY
+import pytest
+
 
 def test_allcoint():
     todo = AllCoinApiIo()
@@ -7,3 +9,26 @@ def test_allcoint():
     todo.getAllCoins(APIKEY)
     total = len(todo.lista_criptos) + len(todo.lista_no_criptos)
     assert total == 16379
+
+def test_exchange():
+    cambio = Exchange("ETH")
+    cambio.updateExchange(APIKEY)
+    assert cambio.rate > 0
+    assert cambio.rate != None
+
+def text_exchange_error():
+    cambio2 = Exchange("ÑIÑO")
+    with pytest.raises(ModelError) as exceptionInfo:
+        cambio2.updateExchange(APIKEY)
+    assert str(exceptionInfo.value) ==  "status: 550, error: You requested specific single item that we don't have at this moment."
+    assert cambio2.status_code == 550
+
+
+
+
+
+
+
+
+
+
