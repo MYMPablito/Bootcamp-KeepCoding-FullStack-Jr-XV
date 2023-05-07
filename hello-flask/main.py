3868962ce127323e330ff966f30f739f95890362
MYMPablito <pablo-mato@hotmail.com>
2023-05-07 00:52:07 -0300

    templates en flask y ejercicio calculadora

--- hello-flask/main.py
+++ hello-flask/main.py
@@ -1,5 +1,5 @@
 #importar la librería de flask
-from flask import Flask
+from flask import Flask, render_template
 
 #inicializar la variable app con flask
 app = Flask(__name__)
@@ -25,7 +25,7 @@ def hola_mundo():
 # Ejercicio una ruta que devuelva una lista de frutas, el path sería /frutas:
 @app.route("/frutas")
 def lista_frutas():
-    list_fruta = ["Banana","Uva","Melon","Naranja","Pomelo"]
+    list_fruta = ["Banana","Uva","Melón","Naranja","Pomelo"]
     return list_fruta
 
 # Ejemplo para enviar parámetros en las rutas:
@@ -40,3 +40,22 @@ def tunombre(n):
 def cuadrado(parametro):
     #parametro = int(parametro)
     return f"El caudrado de {parametro} es {parametro * parametro}"
+
+#Ejercicio 3, realizar una ruta, que dinámicamente pueda solicitar o realizar
+#operaciones de suma,resta, multiplicación y división según los parámetros pasados en la ruta.
+
+@app.route("/operaciones/<float:n1>/<float:n2>/<string:ope>")
+def calculadora(n1,n2,ope):
+    if ope == "suma":
+        return f"La suma de {n1} y {n2} es: {n1+n2}"
+    elif ope == "resta":
+        return f"La resta de {n1} y {n2} es: {n1-n2}"
+    elif ope == "multiplicación":
+        return f"El producto de {n1} y {n2} es: {n1*n2}"
+    elif ope == "división":
+        return f"La división de {n1} y {n2} es: {n1/n2}"
+    
+@app.route("/<nombre>") #esta sería la ruta principal "/", va directo a http://127.0.0.1:5000/ (localhost:5000)
+def llamarhtml(nombre):
+    list_fruta = ["Platano","Fresa","Piña","Melon","Naranja"]
+    return render_template("hola.html",name=nombre,fruts=list_fruta)
