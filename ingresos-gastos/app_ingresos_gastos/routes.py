ca57f5e08b755886da37e0715b16c4c4cc781155
MYMPablito <pablo-mato@hotmail.com>
2023-05-17 07:41:34 -0300

    Últimos cambios registro

--- ingresos-gastos/app_ingresos_gastos/routes.py
+++ ingresos-gastos/app_ingresos_gastos/routes.py
@@ -1,27 +1,43 @@
 from app_ingresos_gastos import app
-from flask import render_template
+from flask import render_template, request, redirect
+import csv
+from datetime import date
 
 @app.route("/")
 def index():
-    datos = [
-        {'fecha':'01/02/2023',
-        'concepto':'Ropa',
-        'monto': '-150'},
-           
-        {'fecha':'01/03/2023',
-        'concepto':'Salario',
-        'monto': '1500'},
-            
-        {'fecha':'15/03/2023',
-        'concepto':'Gastos del super',
-        'monto': '-200'}   
-    ]
+    datos = []
+    #Llamada al archivo:
+    fichero = open('data/movimientos.csv','r')
+    #Accediendo a cada registro del archivo y lo formatea.
+    csvReader = csv.reader(fichero,delimiter=",",quotechar='"')
+    #Recorrer el objeto csvReader y cargar cada registro en la lista datos.
+    for item in csvReader:
+        datos.append(item)
 
     return render_template("index.html", data = datos, title = "Lista")
 
-@app.route("/new")
+@app.route("/new", methods = ["GET","POST"])
 def create():
-    return render_template("new.html", title = "Registro", tipoAccion = "Registro", tipoBoton = "Guardar")
+    if request.method == "POST":
+
+        hoy = str(date.today())
+        
+        if request.form['fecha'] > hoy:
+            return render_template("new.html", title = "Registro", tipoAccion = "Registro", tipoBoton = "Guardar")
+
+        if request.form['fecha'] <= hoy: 
+            # Acceder al archivo y configuarlo para cargar un nuevo registro:
+            mifichero = open('data/movimientos.csv','a',newline='')
+
+            # Llamar al método writer() de escritura y configuramos el formato:
+            lectura = csv.writer(mifichero,delimiter=',',quotechar='"')
+
+            # Registramos los datos recibidos:
+            lectura.writerow([request.form['fecha'],request.form['concepto'],request.form['monto']])
+
+            return redirect("/")
+    else:
+        return render_template("new.html", title = "Registro", tipoAccion = "Registro", tipoBoton = "Guardar")
 
 @app.route("/update")
 def edit():
--- ingresos-gastos/ejemplos.py
+++ ingresos-gastos/ejemplos.py
@@ -14,6 +14,8 @@ resultado = open('data/movimientos.csv','r')
 lectura = resultado.readlines() #lee todas las líneas, guarda como lista.
 print(lectura)
 '''
+
+'''
 import csv
 
 mi_dato = []
@@ -25,5 +27,21 @@ for item in lectura:
     mi_dato.append(item)
 
 print("Mi lista: ", mi_dato[0][0]) #puedo darle posiciones mi_dato[0,1,2,etc...]
+'''
+
+# Ejemplo de registro de datos en csv:
+'''
+import csv
+mifichero = open('data/movimientos.csv','a',newline='')
+lectura = csv.writer(mifichero,delimiter=',',quotechar='"')
+lectura.writerow(['08/12/2023','Nuevo y mejor empleo','15000000'])
+
+mifichero.close()
+'''
+
+#Para obtener la fecha actual:
+
+from datetime import date
+print("fecha de hoy: ", date.today())
 
 
